11adbdbd5d3d78159d592af0fdecbe301aff3b50
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 01:23:31 -0400

    Add image to model

--- src/models/pet.model.js
+++ src/models/pet.model.js
@@ -8,6 +8,10 @@ const PetSchema = Schema(
 		name: { type: String, required: true },
 		age: { type: Number, required: true },
 		sex: { type: String, enum: sexEnum, required: true },
+		image: {
+			type: String,
+			default: 'https://i0.pngocean.com/files/15/838/326/dog-paw-logo-dog.jpg',
+		},
 		owner: { type: Schema.ObjectId, ref: 'User', required: true },
 		created_at: { type: Date },
 		updated_at: { type: Date },
